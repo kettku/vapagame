f47aa922de10faa81ddacca5f4343880550049bf
kettku <katrina.saarenpaa@gmail.com>
2025-12-26 13:27:21 +0200

    fdsf

--- docs/_app/immutable/entry/start.BKM5Ad1k.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/CcExBLYj.js";export{o as load_css,r as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { goto } from '$app/navigation';
+    import { base } from '$app/paths';
 </script>
 
 <h1>Väpän joulumysteeri</h1>
-<button on:click={goto('/intro')}>Aloita peli!</button>
-
+<button on:click={goto(`${base}/intro`)}>Aloita peli!</button>
